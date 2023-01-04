2504e422f1b95300f2337735e7decc74295acd64
U-BSD\394554 <394554@SHS-DGCRCS2.beaverton.k12.or.us>
2023-01-04 13:01:49 -0800

    Files

---
+++ linkedlists/node.cpp
@@ -0,0 +1,15 @@
+#include "node.h"
+
+Node::Node() {
+}
+
+Node::Node(Student* newstudent) {
+
+}
+
+Node::setNext(Node* newnext) {
+  next = newnext;
+}
+
+Node::~Node() {
+}
---
+++ linkedlists/node.h
@@ -0,0 +1,19 @@
+#ifndef NODE_H
+#define NODE_H
+#include <iostream>
+#include <cstring>
+
+class Node {
+ public:
+  Node();
+  //Node(Student*);
+  Node* getNext();
+  //Student* getStudent();
+  void setNext(Node*);
+  ~Node();
+ private:
+  //Student* student;
+  Node* next;
+};
+
+#endif
