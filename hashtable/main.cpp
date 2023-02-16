ca4f2f185795beb1172e8629a4e67bbf0775b916
U-BSD\394554 <394554@SHS-DGCRCS2.beaverton.k12.or.us>
2023-02-16 09:08:26 -0800

    finish hash table resizing

--- hashtable/main.cpp
+++ hashtable/main.cpp
@@ -7,28 +7,30 @@
 #include <fstream>
 #include <random>
 
-//add a student
-void addStudent(Node** hashTable, Node* newn, int size, int h) {
-  //hash
+//add a student to the given slot h
+void addStudent(Node** hashTable, Node* newn, int size, int h, bool& atLimit) {
+  //slot is empty
   if (hashTable[h] == NULL) {
     hashTable[h] = newn;
   }
+  //collision, add to end of linked list
   else {
+    int count = 0; //check if there are more than 3 collisions
     Node* current = hashTable[h];
     while (current->getNext() != NULL) {
+      ++count;
       current = current->getNext();
     }
     current->setNext(newn);
+
+    if (count > 2) atLimit = true;
   }
 }
 
 //print all students in the list
 void print(Node** hashTable, int size) {
-  int count = 0;
-
-  
   for (int i = 0; i < size; ++i) {
-    if (hashTable[i] != NULL) {
+    if (hashTable[i] != NULL) { //slot is not empty
       std::cout << i << "\n";
 
       
@@ -37,21 +39,35 @@ void print(Node** hashTable, int size) {
       while (current->getNext() != NULL) {
 	current->getStudent()->display();
 	current = current->getNext();
-	++count;
       }
 
+      //last node
       if (current->getNext() == NULL) {
 	current->getStudent()->display();
-	++count;
       }
     }
   }
-  std::cout << count << "\n";
 }
 
 //delete a student given their student id
-void deleteStudent() {
-  
+void deleteStudent(Node** hashTable, int id, int size) {
+  for (int i = 0; i < size; ++i) {
+    Node* current = hashTable[i];
+    Node* prev = NULL;
+    while (current != NULL) { //check chain in current array slot
+      if (current->getStudent()->getID() == id) { //match
+	if (prev != NULL) prev->setNext(current->getNext());
+	else hashTable[i] = current->getNext(); //new first node in slot
+	delete current;
+	return;
+      }
+      else {
+	prev = current;
+	current = current->getNext();
+      }
+    }	
+
+  }
 }
 
 //hash string of first name into a number up to size
@@ -61,13 +77,16 @@ int hash(char* name, int size) {
   unsigned int factorpow = 1;
   unsigned int mod = 1e9 + 9;
 
+  //string length
   int len = (sizeof(name))/(sizeof(name[0]));
 			     
   
   for (int i = 0; i < len; ++i) {
-    h = (h + (name[i] - 'a' + 1)*factorpow) % mod;
+    h = (h + (name[i] - 'a' + 1)*factorpow) % mod; //polynomial hash
     factorpow = (factorpow*factor) % mod;
   }
+
+  //adjust to array size
   std::cout << h % size << "\n";
   return h % size;
 }
@@ -75,7 +94,7 @@ int hash(char* name, int size) {
 
 
 //randomly generate the given number of students
-int generate(Node** hashTable, int num, int id) {
+void generate(Node** hashTable, int num, int id, int size, bool& atLimit) {
   for (int i = 0; i < num; ++i) {
     char fname[20], lname[20];
 
@@ -100,26 +119,22 @@ int generate(Node** hashTable, int num, int id) {
     }
     ifs >> lname;
     ifs.close();
-    //int gpa = std::uniform_real_distribution<> dis(0, 4);
 
-    std::cout << fname << " " << lname << "\n";
-    int index = hash(fname, 200);
+    //generate random gpa from 1 to 4
+    std::random_device rd;
+    std::default_random_engine gen(rd());
+    std::uniform_real_distribution<> distribution(1.0, 4.0);
+    double gpa = distribution(gen);
 
+    //hash
+    int index = hash(fname, size);
 
-    Student* news = new Student(fname, lname, id++, 4);
+    Student* news = new Student(fname, lname, id++, gpa);
     Node* newn = new Node(news);
 
-    addStudent(hashTable, newn, 200, index);
+    addStudent(hashTable, newn, size, index, atLimit);
 
   }
-  
-  for (int i = 0; i < 200; ++i) {
-    std::cout << (hashTable[i] != NULL) << " ";
-    if (i % 20 == 0 && i) std::cout << "\n";
-  }
-  std::cout << "\n";
-
-  return id;
 }
 
 
@@ -131,31 +146,41 @@ int main() {
   srand(time(NULL));
   
   char input[10];
-  Node* head = NULL;
+  int id = 1;
+  int size = 200;
+  bool atLimit = false;
 
-  Node* hashTable[200];
-  for (int i = 0; i < 200; ++i)
+  Node** hashTable = new Node*[size];
+  for (int i = 0; i < size; ++i)
     hashTable[i] = NULL;
   
-  int id = 1;
 
   
+  
   while (true) {
+    //need to increase array size to double
+    if (atLimit) {      
+      for (int i = 0; i < size; ++i) {
+	Node* current = hashTable[i];
+
+      }
+
+      size *= 2;
+     
+
+
+      delete [] hashTable;
+      hashTable = new Node*[size];
+      for (int i = 0; i < size; ++i) hashTable[i] = NULL;
+      atLimit = false;
+    }
+
+    
     std::cout << "Enter a command: (ADD, DELETE, PRINT, GEN, QUIT)\n";
     std::cin >> input;
 
     if (strncmp(input, "QUIT", 4) == 0) {
       //delete all remaining nodes if list isn't empty
-      if (head != NULL) {
-	Node* current = head;
-	Node* prev;
-	while (current->getNext() != NULL) {
-	  prev = current;
-	  current = current->getNext();
-	  delete prev;
-	}
-	delete current;
-      }
       
       break;
     }
@@ -164,17 +189,18 @@ int main() {
       int num;
       std::cout << "Enter the number of students to be generated:\n";
       std::cin >> num;
-      id = generate(hashTable, num, id);
-
-      for (int i = 0; i < 200; ++i) {
+      generate(hashTable, num, id, size, atLimit);
+      id += num;
+      
+      for (int i = 0; i < size; ++i) {
 	if (hashTable[i] != NULL) {
 	  std::cout << i << " ";hashTable[i]->getStudent()->display();
 	}
       }
     }
-    /*if (strncmp(input, "ADD", 3) == 0) {
+    if (strncmp(input, "ADD", 3) == 0) {
       //get student info
-      char fname[10], lname[10];
+      char fname[20], lname[20];
       int id;
       float gpa;
       std::cout << "Enter the student's first name:\n";
@@ -189,32 +215,21 @@ int main() {
       //initialize student and node
       Student* news = new Student(fname, lname, id, gpa);
       Node* newn = new Node(news);
-      
-      //first node to be added
-      if (head == NULL) head = newn;
-      //add normally
-      else addStudent(head, head, newn);
-      std::cout << "Student " << id << " has been added to the list.\n";
-    }
-    else if (head == NULL) {
-      std::cout << "No students could be found.\n";
-      continue;
-    }
+      int index = hash(fname, size);
 
-    if (strncmp(input, "PRINT", 5) == 0) {
-      print(head);
+      addStudent(hashTable, newn, size, index, atLimit);
     }
 
     if (strncmp(input, "DELETE", 6) == 0) {
       int id;
       std::cout << "Enter the id of the student to be deleted:\n";
       std::cin >> id;
-      deleteStudent(head, head, id);
+      deleteStudent(hashTable, id, size);
       std::cout << "Student " << id << " has been removed from the list.\n";
-      }*/
+    }
 
     if (strncmp(input, "PRINT", 5) == 0) {
-      print(hashTable, 200);
+      print(hashTable, size);
     }
   }
   
