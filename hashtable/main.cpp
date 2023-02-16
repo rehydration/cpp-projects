cc12bdf26a1255ee47cef377e16c9e57a436d734
U-BSD\394554 <394554@SHS-DGCRCS2.beaverton.k12.or.us>
2023-02-16 07:42:05 -0800

    second

--- hashtable/main.cpp
+++ hashtable/main.cpp
@@ -7,16 +7,46 @@
 #include <fstream>
 #include <random>
 
-//add a student based on student id in ascending order
-void addStudent(Node** hashTable) {
+//add a student
+void addStudent(Node** hashTable, Node* newn, int size, int h) {
   //hash
-
-  
+  if (hashTable[h] == NULL) {
+    hashTable[h] = newn;
+  }
+  else {
+    Node* current = hashTable[h];
+    while (current->getNext() != NULL) {
+      current = current->getNext();
+    }
+    current->setNext(newn);
+  }
 }
 
 //print all students in the list
-void print() {
+void print(Node** hashTable, int size) {
+  int count = 0;
+
   
+  for (int i = 0; i < size; ++i) {
+    if (hashTable[i] != NULL) {
+      std::cout << i << "\n";
+
+      
+      Node* current = hashTable[i];
+      
+      while (current->getNext() != NULL) {
+	current->getStudent()->display();
+	current = current->getNext();
+	++count;
+      }
+
+      if (current->getNext() == NULL) {
+	current->getStudent()->display();
+	++count;
+      }
+    }
+  }
+  std::cout << count << "\n";
 }
 
 //delete a student given their student id
@@ -24,29 +54,40 @@ void deleteStudent() {
   
 }
 
-int hash(std::string name, int size) {
+//hash string of first name into a number up to size
+int hash(char* name, int size) {
   unsigned int h = 0;
-  int factor = 53;
-  int factorpow = 1;
-  int mod = 1e9 + 9;
+  unsigned int factor = 53;
+  unsigned int factorpow = 1;
+  unsigned int mod = 1e9 + 9;
 
-  for (char c : name) {
-    h = (h + (c - 'a' + 1)*factorpow) % mod;
+  int len = (sizeof(name))/(sizeof(name[0]));
+			     
+  
+  for (int i = 0; i < len; ++i) {
+    h = (h + (name[i] - 'a' + 1)*factorpow) % mod;
     factorpow = (factorpow*factor) % mod;
   }
   std::cout << h % size << "\n";
-
+  return h % size;
 }
+
+
+
 //randomly generate the given number of students
-void generate(int num, int id) {
+int generate(Node** hashTable, int num, int id) {
   for (int i = 0; i < num; ++i) {
     char fname[20], lname[20];
-    int line = (rand() % 10000);
+
+    //generate random line numbers
+    int fnameLine = (rand() % 10000);
+    int lnameLine = (rand() % 48000);
+
     std::ifstream ifs;
 
     //get first name
     ifs.open("fnames.txt");
-    for (int j = 0; j < line-1; ++j) {
+    for (int j = 0; j < fnameLine-1; ++j) { //continue until reaching fnameLine
       ifs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
     ifs >> fname;
@@ -54,7 +95,7 @@ void generate(int num, int id) {
 
     //get last name
     ifs.open("lnames.txt");
-    for (int j = 0; j < line-1; ++j) {
+    for (int j = 0; j < lnameLine-1; ++j) {
       ifs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
     ifs >> lname;
@@ -62,8 +103,23 @@ void generate(int num, int id) {
     //int gpa = std::uniform_real_distribution<> dis(0, 4);
 
     std::cout << fname << " " << lname << "\n";
-    hash(fname, 200);
+    int index = hash(fname, 200);
+
+
+    Student* news = new Student(fname, lname, id++, 4);
+    Node* newn = new Node(news);
+
+    addStudent(hashTable, newn, 200, index);
+
   }
+  
+  for (int i = 0; i < 200; ++i) {
+    std::cout << (hashTable[i] != NULL) << " ";
+    if (i % 20 == 0 && i) std::cout << "\n";
+  }
+  std::cout << "\n";
+
+  return id;
 }
 
 
@@ -78,7 +134,10 @@ int main() {
   Node* head = NULL;
 
   Node* hashTable[200];
-
+  for (int i = 0; i < 200; ++i)
+    hashTable[i] = NULL;
+  
+  int id = 1;
 
   
   while (true) {
@@ -105,9 +164,14 @@ int main() {
       int num;
       std::cout << "Enter the number of students to be generated:\n";
       std::cin >> num;
-      generate(num, 1);
+      id = generate(hashTable, num, id);
+
+      for (int i = 0; i < 200; ++i) {
+	if (hashTable[i] != NULL) {
+	  std::cout << i << " ";hashTable[i]->getStudent()->display();
+	}
+      }
     }
-    
     /*if (strncmp(input, "ADD", 3) == 0) {
       //get student info
       char fname[10], lname[10];
@@ -147,12 +211,11 @@ int main() {
       std::cin >> id;
       deleteStudent(head, head, id);
       std::cout << "Student " << id << " has been removed from the list.\n";
-    }
-
-    if (strncmp(input, "AVERAGE", 7) == 0) { //to 2 decimals
-      std::cout << "The average GPA is " << std::fixed << std::setprecision(2) << average(head, 0, 0) << "\n";
       }*/
 
+    if (strncmp(input, "PRINT", 5) == 0) {
+      print(hashTable, 200);
+    }
   }
   
   return 0;
