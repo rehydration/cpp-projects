ea96984b6aa99172efc5dc60103910a597ffcfeb
U-BSD\394554 <394554@SHS-DGCRCS2.beaverton.k12.or.us>
2023-03-02 09:12:15 -0800

    fix

--- heap/main.cpp
+++ heap/main.cpp
@@ -1,7 +1,13 @@
+/*
+ */
+
+
 #include <iostream>
 #include <fstream>
 #include <cstring>
 
+
+//insert node at given index
 void insert(int* tree, int node, int index) {
   tree[index] = node;
   while (index/2 && tree[index/2] < node) { //swap parent and node
@@ -17,23 +23,22 @@ void remove(int* tree) {
   int node = 1;
   std::cout << tree[node] << "\n";
 
-  while (true) {
+  do { //sift up elements
     
-    if (tree[2*node] > tree[2*node+1]) {
+    if (tree[2*node] > tree[2*node+1]) { //left
       tree[node] = tree[2*node];
       node = 2*node;
       tree[node] = 0;
     }
-    else {
+    else { //right
       tree[node] = tree[2*node+1];
       node = 2*node+1;
       tree[node] = 0;
     }
-
-    if (!tree[2*node] && !tree[2*node+1]) return;
-  }
+  } while (tree[2*node] || tree[2*node+1]);
 }
 
+//remove all elements from heap
 void remove_all(int* tree) {
   while (tree[1] != 0) {
     remove(tree);
@@ -41,50 +46,28 @@ void remove_all(int* tree) {
 }
 
 //output the heap
-void display(const int* tree, int* layer, int l) {
-  std::cout << l << ": ";
-  int* newlayer = new int[101];
-  int cnt = 0;
-  for (int i = 0; i <= 100; ++i) newlayer[i] = 0;
-
-  bool valid = false; //next layer has nonzero
-  for (int i = 0; i < 100; ++i) {
-    if ((layer[i] || tree[layer[i]])) { //index and value both valid
-      std::cout << tree[layer[i]] << "\t";
-
-
-      newlayer[cnt] = 2*layer[i];
-      ++cnt;
-      if (tree[2*layer[i]] > 0) { //child 1
-	
-	valid = true;
-      }
-
-      
-      newlayer[cnt] = 2*layer[i]+1;
-      ++cnt;
-      if (tree[2*layer[i]+1] > 0) { //child 2
-	valid = true;
-      }
-    }
-      
-    else break;
+void display(const int* tree, int ind, int depth) {
+  if (tree[2*ind+1]) { //right node
+    display(tree, 2*ind+1, depth+1);
   }
-  std::cout << "\n";
-  layer = newlayer;
-  if (valid) display(tree, layer, ++l);
+  
+  for (int i = 0; i < depth; ++i) std::cout << '\t';
+  std::cout << tree[ind] << "\n";
+
+  if (tree[2*ind]) { //left node
+    display(tree, 2*ind, depth+1);
+  }
+  
 }
 
 
 int main() {
-  int tree[101];
-  bool vis[101];
+  int tree[102];
   char input[20];
   bool running = true;
 
   for (int i = 1; i <= 100; ++i) {
     tree[i] = 0;
-    vis[i] = false;
   }
 
   int size = 1;
@@ -94,9 +77,12 @@ int main() {
   std::cin >> num;
   std::cout << "Enter nodes\n";
 
+  std::cout << "Choose input method ('1' for manual entry; '2' for file)\n";
+  
+  char file_name[20] = "nums2";
   
   std::ifstream ifs;
-  ifs.open("nums.txt");
+  ifs.open(std::strcat(file_name, ".txt"));
   for (int i = 1; i <= num; ++i) {
     //std::cin >> n;
     ifs >> n;
@@ -105,10 +91,10 @@ int main() {
   }
   ifs.close();
 
-  int* h = new int[101];
-  for (int i = 0; i < 100; ++i) h[i] = 0;
-  h[0] = 1;
-  display(tree, h, 1);
+  for (int i = 1; i <= 100; ++i) std::cout << tree[i] << " ";
+
+  
+  display(tree, 1, 0);
   //remove(tree);
   //display(tree, h, 1);
 
