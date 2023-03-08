511f21b49fca1c887926df4ec307133813603cf7
U-BSD\394554 <394554@SHS-DGCRCS2.beaverton.k12.or.us>
2023-03-08 09:12:10 -0800

    stack and queue

--- shuntingyard/main.cpp
+++ shuntingyard/main.cpp
@@ -5,35 +5,37 @@
 #include <iostream>
 #include <cstring>
 #include <cctype>
+#include "btnode.h"
 
 
 struct Node {
-  char data = '\t';
-  Node* next = NULL;
+  char data;
+  bool empty;
+  Node* next = nullptr;
 };
 
 //stack functions
 
 void push(Node*& head, char t) {
-  if (head->data != '\t') {
-    Node* newh = new Node;
-    newh->data = t;
-    newh->next = head;
-    head = newh;
-    //std::cout << t << "\n";
-  }
-  else {
-    Node* h;
-    h->data = t;
-    head = h;
-    std::cout << t << "\n";
-  }
+  
+  Node* newh = new Node;
+  newh->data = t;
+  newh->empty = false;
+  if (head != nullptr) newh->next = head;
+  head = newh;
+
+  //std::cout << head->data << " " << head->empty << " " << (head->next == nullptr) << "\n";
+
 }
 
 char pop(Node*& head) {
+  //if (head->empty) std::cout << "No items remaining\n";
+  
   Node* temp = head;
   char value = temp->data;
   head = head->next;
+  //if (head == nullptr) head->empty = true;
+
   delete temp;
   return value;
 }
@@ -45,35 +47,39 @@ Node* peek(Node*& head) {
 
 //queue functions
 
-void enqueue(Node*& queue_head, Node*& queue_end, char t) {
-  if (queue_end->data != ' ') {
-    Node* newn = new Node;
-    newn->data = t;
-    queue_end->next = newn;
-    queue_end = newn;
+void enqueue(Node*& head, Node*& end, char t) {
+
+  Node* newn = new Node;
+  newn->data = t;
+  newn->empty = false;
+  
+  if (head == nullptr) { //first node
+    head = newn;
   }
-  else {
-    Node* newn;
-    newn->data = t;
-    queue_end = newn;
-    queue_head = newn;
-  }
+  else end->next = newn;
+  end = newn;
 }
 
-char dequeue(Node* queue_head) {
-  Node* temp = queue_head;
+char dequeue(Node*& head) {
+  Node* temp = head;
   char value = temp->data;
-  queue_head = queue_head->next;
+  head = head->next;
+
   delete temp;
   return value;
 }
 
 
 int main() {
-  Node* stack_head;
-  Node* queue_head;
-  Node* queue_end;
-
+  Node* stack_head = nullptr;
+  Node* queue_head = nullptr;
+  Node* queue_end = nullptr;
+  //BTNode* tree_head;
+  
+  /*stack_head->empty = true;
+  queue_head->empty = true;
+  queue_end->empty = true;
+  */
   //bool running = true;
   
   std::cout << "Enter an expression: (end with 'x')\n";
@@ -92,15 +98,17 @@ int main() {
     }
 
   }
-  //std::cout << stack_head
-  Node* h = stack_head;
-  while (h->next != NULL) {
-    std::cout << h->data << "\n";
-    h = h->next;
+  //std::cout << pop(stack_head);
+  //std::cout << stack_head->data << " " << stack_head->next->data;
+  
+  while (stack_head != nullptr) {
+    std::cout << pop(stack_head) << "\n";
+    //h = h->next;
   }
-
-  while (queue_head->next != NULL) dequeue(queue_head);
-
+  
+  //std::cout << queue_head->data << "\n";
+  while (queue_head != nullptr) std::cout << dequeue(queue_head) << "\n";
+  
   
   return 0;
 }
